ed6c686992f82f4c0e3ec03d467f602a41fdf075
Daniel Bauer <danniccs@gmail.com>
2020-08-04 06:36:01 -0300

    Started on normal visualization (have to use notebook to execute) but it is not being visualized as expected

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 import kaolin as kal
 import numpy as np
+import ipyvolume as ipv
 from PIL import Image
 
 def compute_face_areas(mesh):
@@ -42,3 +43,18 @@ def export_normals_as_texture(vertex_normals, image_path="normal_texture.png"):
     image = Image.fromarray(np_color_norms, mode='RGB')
     # Guardo la imágen
     image.save(image_path)
+
+def visualize_normals(mesh, normal_texture):
+    # normal_texture debe ser una textura de num_vertices * 1. Construyo el mapa de coordenadas uv
+    u = np.array([i/mesh.vertices.size(0) for i in range(0, mesh.vertices.size(0))])
+    v = 0.5 * np.ones(mesh.vertices.size(0))
+    # Convierto las coordenadas de vertices al formato esperado por ipyvolume
+    x = mesh.vertices[:, 0].numpy()
+    y = mesh.vertices[:, 1].numpy()
+    z = mesh.vertices[:, 2].numpy()
+    # Defino las caras en el formato esperado por ipyvolume
+    faces = mesh.faces.numpy()
+    # Dibujo el mesh con la textura
+    ipv.figure()
+    ipv_mesh = ipv.plot_trisurf(x, y, z, triangles=faces, u=u, v=v, texture=normal_texture)
+    ipv.show()
